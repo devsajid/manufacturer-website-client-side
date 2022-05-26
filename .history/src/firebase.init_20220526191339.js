41c2b841dc5b70d2c348634d623f3acf8e8a8ff8
devsajid <74415469+devsajid@users.noreply.github.com>
2022-05-26 20:07:30 +0600

    setup

---
+++ .history/src/firebase.init_20220526191339.js
@@ -0,0 +1,50 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+
+//   // apiKey: process.env.REACT_APP_apiKey,
+//   // authDomain: process.env.REACT_APP_authDomain,
+//   // projectId: process.env.REACT_APP_projectId,
+//   // storageBucket: process.env.REACT_APP_storageBucket,
+//   // messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//   // appId: process.env.REACT_APP_appId
+
+//   apiKey: "AIzaSyCubrbBuuhz5IKzVpsZWfcUzcS1rdq0_fk",
+//   authDomain: "phone-plus-4f5d3.firebaseapp.com",
+//   projectId: "phone-plus-4f5d3",
+//   storageBucket: "phone-plus-4f5d3.appspot.com",
+//   messagingSenderId: "1050316587029",
+//   appId: "1:1050316587029:web:675504103bcda3d9a8ce1c"
+
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// export default auth;
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAnm3L9qxeep06RzjsloHUwBWJT0eqF0Vo",
+  authDomain: "car-manufacturer-app.firebaseapp.com",
+  projectId: "car-manufacturer-app",
+  storageBucket: "car-manufacturer-app.appspot.com",
+  messagingSenderId: "842670612530",
+  appId: "1:842670612530:web:a21eb6fefe76514fd65c2b",
+  measurementId: "G-M21YL917YE",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ src/Components/Loading/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div className='flex items-center justify-center'>
+            <button class="btn no-animation">I don't have click animation</button>
+        </div>
+
+    )
+}
+
+export default Loading
